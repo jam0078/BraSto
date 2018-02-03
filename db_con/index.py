4dfba7a6434898ca653aa415787af8e30060eb84
hiraga <jam0078g@gmail.com>
2018-02-03 17:43:21 +0900

    add decimal_default_proc

--- db_con/index.py
+++ db_con/index.py
@@ -1,9 +1,15 @@
 import boto3
 import json
 from boto3.dynamodb.conditions import Key, Attr
+from decimal import Decimal
+
+def decimal_default_proc(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError
 
 def handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table    = dynamodb.Table('sessions')
     response = table.scan()['Items'][0]
-    return json.dumps(response)
+    return json.dumps(response, default=decimal_default_proc)
