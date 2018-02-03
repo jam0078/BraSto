819d7433e56fb9eef99c6e6c8adafd29cdd39991
hiraga <jam0078g@gmail.com>
2018-02-03 14:33:17 +0900

    dynamo scan test

--- db_con/index.py
+++ db_con/index.py
@@ -1,3 +1,9 @@
+import boto3
+import json
+from boto3.dynamodb.conditions import Key, Attr
+
 def handler(event, context):
-    # TODO implement
-    return 'Hello from Lambda'
+    dynamodb = boto3.resource('dynamodb')
+    table    = dynamodb.Table('sessions')
+    response = table.scan()
+    return response
