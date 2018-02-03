e05cb4d4a1727fb0a94e6904c71ef61ef743deae
hiraga <jam0078g@gmail.com>
2018-02-03 14:54:52 +0900

    DynamoDBのsessionsの一つ目の要素を取得

--- db_con/index.py
+++ db_con/index.py
@@ -5,5 +5,5 @@ from boto3.dynamodb.conditions import Key, Attr
 def handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table    = dynamodb.Table('sessions')
-    response = table.scan()
+    response = table.scan()['Items'][0]
     return response
