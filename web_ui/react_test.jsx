55d469cbebced5923d7daaa8c9263fd99e8059f0
hiraga <jam0078g@gmail.com>
2018-02-03 18:30:10 +0900

    test

--- web_ui/react_test.jsx
+++ web_ui/react_test.jsx
@@ -1,8 +1,11 @@
 class TestComponent extends React.Component {
-    constructor() { this.state = {}; }
     render() {
         return (
-            <div dangerouslySetInnerHTML={{__html: 'First &middot; Second'}} />
+            // <div dangerouslySetInnerHTML={{__html: '<h1>Hello</h1>'}} />
+            <div>
+                <h1>Hello,React!</h1>
+                <h2>{(new Date()).toString()}</h2>
+            </div>
         )
     }
 }
